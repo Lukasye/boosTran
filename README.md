572e658d67aba087da577b1ffeca2477d6cd1c99
Lukasye <yun.ye.lukas@gmail.com>
2023-06-24 10:23:03 +0200

    update

--- README.md
+++ README.md
@@ -5,10 +5,18 @@ A little idea about adaboost
 Another tiny little project. The idea is that in adaboost, the optimization of the whole set of weights and weak learner function is infeasible, so an iterative method is needed. But the Iterative method is somehow not that intuitive and efficient. So I wanna find a way to minimize the Loss function in a global sense. <br>
 The basic idea for adaboost is we regard the weak learner as somehow a bunch of basis, and find a suiable weights for all of them. So, the assumption would be, there are a sets of weak learner function at the beginning, and the problem will reduced to the region of linear algebra.
 
+## Dataset
+I used the [Coffee : EDA](https://www.kaggle.com/code/tumpanjawat/coffee-eda-geo-cluster-regression#2-%7C-Exploratory-Data-Analysis-) from kaggle as in the lecture does.
+
 ## Impementation
 If we want this algorithm to preform well, we need to make sure that the basis is as uncorrelated as possible. Hence we can apply following method to ensure this property:<br>
 1. Boosting sampling of data (or even features)
 2. Postprocessing like Grand-schmidt or PCA (QR decomposition)
 3. Check the rank of the outcome.
+
+
+With the implementation of Boostrap sample, I've got the following result:
+<img src="./img/uncorelated.png" width="400" height="300"> <br>
+We can see that with `bootstrap_ratio=0.1` got the best result, because the samples a the most uncorrelated, so that the final transformation matrix is 'healthy'. And the idea nunmber of weak learner is around 5 to 15. But the outcome is still not good enough. 
 
 Since in the real application, the dataset supposed to be way larger than the number of weak learners. The matrix that we are going to get at the end should have infinitely many solution. So, a psuedoinverse will be calculated.
--- boosTran.py
+++ boosTran.py
@@ -1,32 +1,40 @@
+from tqdm import tqdm
 from sklearn.tree import DecisionTreeClassifier
 from dataUtils import load_dataset
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 class boosTran:
-    def __init__(self, num_learner: int = 30) -> None:
+    def __init__(self, num_learner: int = 30, bootstrap_ratio = 0.9) -> None:
         self.num_learner = num_learner
         self.tree_kwargs = dict(criterion="entropy", class_weight="balanced", max_depth=1)
-        self
+        self.bootstrap_ratio = bootstrap_ratio
     
     
     def fit(self, X, y):
+        # Variable initialization
         length_sample = len(X)
-        weight = np.ones(length_sample) / length_sample
         self.tree = [DecisionTreeClassifier(**self.tree_kwargs) for _ in range(self.num_learner)]
         P = np.empty((length_sample, self.num_learner))
+        sample_index = np.arange(length_sample)
+        bootstrap_length = int(self.bootstrap_ratio * length_sample)
+        weight = np.ones(bootstrap_length) / bootstrap_length
+        # Main loop
         for i in range(self.num_learner):
+            np.random.shuffle(sample_index)
             # weight = np.random.rand(length_sample)
             # weight /= sum(weight)
+            tmp_x = X[sample_index[:bootstrap_length], :]
+            tmp_y = y[sample_index[:bootstrap_length]]
             tmp_tree = self.tree[i]
-            tmp_tree.fit(X, y, sample_weight= weight)
+            tmp_tree.fit(tmp_x, tmp_y, sample_weight= weight)
             y_hat = tmp_tree.predict(X)
-            weight *= np.exp((y != y_hat))
+            weight *= np.exp((y != y_hat)[sample_index[:bootstrap_length]])
             weight /= np.sum(weight)
             P[:, i] = y_hat
         # np.savetxt('matrixTemplate.csv', P, delimiter=',')
         rank = np.linalg.matrix_rank(P)
-        print('rank= ' , rank)
 
         # basis
         self.basis = np.linalg.pinv(P) @ y
@@ -42,16 +50,39 @@ class boosTran:
         return result.astype(int)
 
 
-def main():
+def demo():
     # Prepare dataset
     X_train, X_test, y_train, y_test = load_dataset("df_arabica_clean.csv")
     # Initialize 
-    bs = boosTran(20)
+    bs = boosTran()
     bs.fit(X_train, y_train)
     print(bs.basis)
     yhat = bs.prediction(X_test)
     asd = yhat == y_test
     print(np.sum(asd) / len(yhat))
+
+def main():
+    # Prepare dataset
+    X_train, X_test, y_train, y_test = load_dataset("df_arabica_clean.csv")
+    average = 10
+
+    plt.figure()
+    x = range(1, 80, 5)
+    ratio = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.]
+    for r in tqdm(ratio):
+        result = np.zeros(len(x))
+        # averaging result
+        for _ in range(average):
+            for idx, i in enumerate(x):
+                bs = boosTran(num_learner=i, bootstrap_ratio=r)
+                bs.fit(X_train, y_train)
+                yhat = bs.prediction(X_test)
+                asd = yhat == y_test
+                result[idx] += np.sum(asd) / len(yhat)
+        result /= average
+        plt.plot(x, result)
+    plt.legend(ratio)
+    plt.show()
 
 
 if __name__ == "__main__":
