cf6c6c14d44b25398660879f3fa3a0702c14fa1a
Lukasye <yun.ye.lukas@gmail.com>
2023-06-24 14:36:06 +0200

    almost finish

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ A little idea about adaboost
 ## Introduction
 Another tiny little project. The idea is that in adaboost, the optimization of the whole set of weights and weak learner function is infeasible, so an iterative method is needed. But the Iterative method is somehow not that intuitive and efficient. So I wanna find a way to minimize the Loss function in a global sense. <br>
 The basic idea for adaboost is we regard the weak learner as somehow a bunch of basis, and find a suiable weights for all of them. So, the assumption would be, there are a sets of weak learner function at the beginning, and the problem will reduced to the region of linear algebra.
+Since in the real application, the dataset supposed to be way larger than the number of weak learners. The matrix that we are going to get at the end should have infinitely many solution. So, one of the major problem in this project is to determine the right weight metrics to be used. 
 
 ## Dataset
-I used the [Coffee : EDA](https://www.kaggle.com/code/tumpanjawat/coffee-eda-geo-cluster-regression#2-%7C-Exploratory-Data-Analysis-) from kaggle as in the lecture does.
+I used the [Coffee : EDA](https://www.kaggle.com/code/tumpanjawat/coffee-eda-geo-cluster-regression#2-%7C-Exploratory-Data-Analysis-) from kaggle as in the lecture did. For more information see preprocessing python script `dataUtils.py`.
 
 ## Impementation
 If we want this algorithm to preform well, we need to make sure that the basis is as uncorrelated as possible. Hence we can apply following method to ensure this property:<br>
@@ -28,6 +29,37 @@ Another interesting fact is that, while the complexity of the model is increasin
 <img src="./img/max_deplth3.png" width="400" height="300"> <br>
 
 ### Interesting
-Something else might be fun: In all the model, the model will aquire the highest perfoemance, is around 15 weaklearner(as we increase the expresiveniss). My assumption: it is related to the number of feature that we are using, as the model depends on the number of features that we use, we can't expect a higher DoF than the number of feature, everything greater than 15, in this case, will be overfitting.
+Something else might be fun: In all the model, the model will aquire the highest perfoemance, is around 15 weaklearner(as we increase the expresiveniss). My assumption: it is related to the number of feature that we are using, as the model depends on the number of features that we use, we can't expect a higher DoF than the number of feature, everything greater than 15, in this case, will be overfitting.<br>
 
-Since in the real application, the dataset supposed to be way larger than the number of weak learners. The matrix that we are going to get at the end should have infinitely many solution. So, a psuedoinverse will be calculated.
+So, in the following testing, I set the `num_learner` always to the number of features. And than I compared the influence of the depth of the tree, which the result shows as follow:<br>
+<img src="./img/different_depth_ratio.png" width="400" height="300"> <br>
+The model is underfitting if there arn't enough depth in the weak learner. It can be treateed as a hyperparameter.
+
+
+### Retrieving the weights
+After we've chosen the set of weak learners, the next problem is how to calculate the weights for each one of them, here I propose three simple method:
+
+#### Projection
+The simplest is just to do the projection, we project the final outcome to each of the data column to obtain our final value. And also, we should do the normalization.
+#### Pseudo Inverse
+The first method that I came up with to solve this question is to use the Pseudo Inverse, since the matrix will be normally underdetermined. It can be realized with simple line of code in python:
+```python
+np.linalg.pinv(P) @ y
+```
+#### SVD
+I alse tried to formulate the problem as a optimization problem that we calculate the weights subject to the condition that all the weights add up to one.
+$$loss = (P * w - y)^2 + \lambda (w^T * w) $$
+And after a few steps of calculation, the program reduced to an EWVD problem. Since EVD is not available in this case, I've tried SVD (a stupid mistake), and it finally didn't work out.
+
+#### Another optimization problem
+Finally it occurrs to me that all weights should be positive in adaboost to ensure the correctness in the weight updating formular. So, I reformulated the optimization problem:
+$$\underset{b, \lambda}{\operatorname{argmin}} [\sum_{n=1}^N (y_n - f(x_n)^Tb)^2 - \sum_{i=1}^K\lambda_ib_i]$$
+After a few step I gave up for I have no clue how to solve it after I wrote down the KKT conditions. So, I used the python optimization package to directly calcalate the nummeric solution.
+
+The result of those attampts shows as follows:<br>
+<img src="./img/different_methods.png" width="400" height="300"> <br>
+
+The SVD preforms bad, because my calculations are wrong, other methods haa almost the same accuracy. `opt` is significantly slower as it solves a numeric optimizations problem.
+
+## Benchmark and Conclution
+
--- boosTran.py
+++ boosTran.py
@@ -3,17 +3,29 @@ from sklearn.tree import DecisionTreeClassifier
 from dataUtils import load_dataset
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import minimize
 
+# For linear Regression
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+
+
+# For optimizzation
+def objective(a, P, y):
+    return sum((P.dot(a) - y)**2)
 
 class boosTran:
-    def __init__(self, num_learner: int = 30, bootstrap_ratio = 0.9) -> None:
-        self.num_learner = num_learner
-        self.tree_kwargs = dict(criterion="entropy", class_weight="balanced", max_depth=3)
+    def __init__(self, bootstrap_ratio = 0.9, method: str = 'pinv') -> None:
+        self.tree_kwargs = dict(criterion="entropy", class_weight="balanced", max_depth=2)
         self.bootstrap_ratio = bootstrap_ratio
-    
+        self.method = method
+
     
     def fit(self, X, y):
         # Variable initialization
+        self.num_learner = X.shape[1]
+        self.basis = np.zeros(self.num_learner)
+        self.constraints = [{'type': 'ineq', 'fun': lambda a: a[i]} for i in range(len(self.basis))]
         length_sample = len(X)
         self.tree = [DecisionTreeClassifier(**self.tree_kwargs) for _ in range(self.num_learner)]
         P = np.empty((length_sample, self.num_learner))
@@ -37,8 +49,21 @@ class boosTran:
         rank = np.linalg.matrix_rank(P)
 
         # basis
-        self.basis = np.linalg.pinv(P) @ y
+        if self.method == 'pinv':
+            self.basis = np.linalg.pinv(P) @ y
+        elif self.method == 'svd':
+            _, S, Vh = np.linalg.svd(P)
+            # print("Smallest Eigen value: ", S[rank - 1])
+            # print(Vh[rank - 1, :])
+            self.basis = Vh[rank - 1, :]
+        elif self.method == 'opt':
+            result = minimize(objective, self.basis, args=(P, y), constraints=self.constraints)
+            self.basis = result.x
+        elif self.method == 'proj':
+            for i in range(self.num_learner):
+                self.basis[i] = y.dot(P[:, i])
         self.basis /= np.sum(self.basis)
+
     
     def prediction(self, X):
         length_sample = len(X)
@@ -49,10 +74,13 @@ class boosTran:
         result = (np.sign(result - 0.5) + 1) / 2
         return result.astype(int)
 
-def benchmark(num_learner, bootstrap_ratio, X_train, y_train, X_test, y_test, epoche: int = 10) -> float:
+def benchmark(bootstrap_ratio, max_depth, X_train, y_train, X_test, y_test, 
+              epoche: int = 10,
+              method: str = 'pinv') -> float:
     result = []
     for _ in range(epoche):
-        bs = boosTran(num_learner, bootstrap_ratio)
+        bs = boosTran(bootstrap_ratio, method=method)
+        bs.tree_kwargs['max_depth'] = max_depth
         bs.fit(X_train, y_train)
         yhat = bs.prediction(X_test)
         asd = yhat == y_test
@@ -63,14 +91,14 @@ def demo():
     # Prepare dataset
     X_train, X_test, y_train, y_test = load_dataset("df_arabica_clean.csv")
     # Initialize 
-    bs = boosTran()
+    bs = boosTran(method='proj')
     bs.fit(X_train, y_train)
     print(bs.basis)
     yhat = bs.prediction(X_test)
     asd = yhat == y_test
     print(np.sum(asd) / len(yhat))
 
-def main():
+def compare_ratio_numlearners():
     # Prepare dataset
     X_train, X_test, y_train, y_test = load_dataset("df_arabica_clean.csv")
     average = 10
@@ -82,16 +110,61 @@ def main():
         result = []
         # averaging result
         for idx, i in enumerate(x):
-            # bs = boosTran(num_learner=i, bootstrap_ratio=r)
-            # bs.fit(X_train, y_train)
-            # yhat = bs.prediction(X_test)
-            # asd = yhat == y_test
-            # result[idx] += np.sum(asd) / len(yhat)
-            result.append(benchmark(i, r, X_train, y_train, X_test, y_test, 10))
+            result.append(benchmark(r, X_train, y_train, X_test, y_test, 10))
         plt.plot(x, result)
     plt.legend(ratio)
     plt.show()
 
+def main():
+    # Prepare dataset
+    X_train, X_test, y_train, y_test = load_dataset("df_arabica_clean.csv")
+    average = 10
+
+    plt.figure()
+    x = range(1, 7)
+    ratio = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.]
+    for i in x:
+        result = []
+        for r in tqdm(ratio):
+        # averaging result
+            result.append(benchmark(r, i, X_train, y_train, X_test, y_test, 10, method='opt'))
+        plt.plot(ratio, result)
+    plt.legend(x)
+    plt.show()
+
+
+def linear_regression():
+    # Prepare dataset
+    X_train, X_test, y_train, y_test = load_dataset("df_arabica_clean.csv")
+    model=LinearRegression()
+    model.fit(X_train,y_train)
+    y_pred=model.predict(X_test)
+    y_pred = (np.sign(y_pred - 0.5) + 1)/2
+    y_pred.astype(int)
+    result = y_pred == y_test
+    print(np.sum(result) / len(result))
+
+
+def compare_methods():
+    # Prepare dataset
+    X_train, X_test, y_train, y_test = load_dataset("df_arabica_clean.csv")
+    methods = ['proj', 'svd', 'pinv', 'opt']
+    ratio = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.]
+    
+    plt.figure()
+    for m in methods:
+        result = []
+        for r in tqdm(ratio):
+            result.append(benchmark(r, 4, X_train, y_train, X_test, y_test, 10, method=m))
+        plt.plot(ratio, result)
+    plt.legend(methods)
+    plt.show()
+
+
+
 
 if __name__ == "__main__":
-    main()
+    # main()
+    # demo()
+    # linear_regression()
+    compare_methods()
